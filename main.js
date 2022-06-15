364fd668cf6547f0859dd299b622ab0689c67ba8
Dylan Cade <dacade.code@gmail.com>
2022-06-15 01:09:05 -0600

    polished

--- main.js
+++ main.js
@@ -14,13 +14,20 @@ window.onscroll = function () {
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      if (entry.target.className.includes("red")) {
-        entry.target.classList.add('red-animation')
-      }
       if (entry.target.className.includes("green")) {
+        entry.target.classList.add('visible')
         entry.target.classList.add('green-animation')
       }
-      if (entry.target.className.includes("pop")) {
+      if (entry.target.className.includes("red")) {
+        entry.target.classList.add('visible')
+        entry.target.classList.add('red-animation')
+      }
+      if (entry.target.className.includes("pop1")) {
+        entry.target.classList.add('visible')
+        entry.target.classList.add('pop-up')
+      }
+      if (entry.target.className.includes("pop2")) {
+        entry.target.classList.add('visible')
         entry.target.classList.add('pop-up')
       }
     }
@@ -29,4 +36,5 @@ const observer = new IntersectionObserver(entries => {
 
 observer.observe(document.querySelector('.whitespace.red'))
 observer.observe(document.querySelector('.whitespace.green'))
-observer.observe(document.querySelector('.pop'))
+observer.observe(document.querySelector('.pop1'))
+observer.observe(document.querySelector('.pop2'))
