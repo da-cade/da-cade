ea27ed9fd3a9aaca49c21a2fdc3e316517472373
Dylan Cade <dacade.code@gmail.com>
2022-07-27 13:45:52 -0600

    colcade test

--- assets/js/main.js
+++ assets/js/main.js
@@ -183,6 +183,14 @@
     }
   });
 
+  /**
+   * Portfolio masonry
+   */
+  var grid = document.querySelector('.grid');
+  var colc = new Colcade('.grid', {
+    columns: '.grid-col',
+    items: '.grid-item'
+  });
 
   /**
    * Porfolio isotope and filter
